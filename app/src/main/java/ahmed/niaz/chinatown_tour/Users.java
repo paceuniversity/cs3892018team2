38ee8f95bdf00a767b0d0c4df89f335eeb0dc12a
WilliamZhong <wz16874n@pace.edu>
2018-04-10 15:27:24 -0400

    Added User Class

---
+++ app/src/main/java/ahmed/niaz/chinatown_tour/Users.java
@@ -0,0 +1,17 @@
+package ahmed.niaz.chinatown_tour;
+
+/**
+ * Created by WilliamZhong on 4/10/18.
+ */
+
+public class Users {
+    String userID;
+    String feedback;
+    String email;
+
+    public Users(String userID, String feedback, String email) {
+        this.userID = userID;
+        this.feedback = feedback;
+        this.email = email;
+    }
+}
