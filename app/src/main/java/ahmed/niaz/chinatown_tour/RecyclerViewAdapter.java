7abf19e1f4007cf622a0fa39893f6212fcf8ddd6
Niaz Ahmed <niaz151@gmail.com>
2018-04-27 18:22:19 -0400

    Food Icon Image Constraint Edit

--- app/src/main/java/ahmed/niaz/chinatown_tour/RecyclerViewAdapter.java
+++ app/src/main/java/ahmed/niaz/chinatown_tour/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package ahmed.niaz.chinatown_tour;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -91,9 +92,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.image.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                // Log.d(TAG, "onClick: clicked on an image" + restaurantNames.get(position));
-               /* Intent intent = new Intent (view.getContext(), SpecificFoodActivity.class);
+               Intent intent = new Intent(view.getContext(), SpecificFoodActivity.class);
                 rContext.startActivity(intent);
-                */
+
                 Log.i("POSITION", (" " + position + " "));
                 Log.i("ARRAY SIZE", (" " + restaurantNames.size() + " "));
                 Log.i("INDEX", ( " " + restaurantNames.indexOf(restaurantNames.get(position)) + " "));
